2fbf992b389a8a72e562d5cce23ad54066f61039
SuperKevinZhou <zzh__2020@outlook.com>
2025-09-29 19:03:15 +0800

    Add onEnable and onDisable info

--- src/main/java/kim/pvp/kevin16/plugins/next_enchantments/NextEnchantments.java
+++ src/main/java/kim/pvp/kevin16/plugins/next_enchantments/NextEnchantments.java
@@ -7,11 +7,12 @@ public final class NextEnchantments extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("NextEnchantments Plugin has been enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("NextEnchantments Plugin has been disabled!");
     }
 }
