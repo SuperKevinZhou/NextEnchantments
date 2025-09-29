ed9a0bdf5ae42d91f778782c4787ad2d8048a6e8
SuperKevinZhou <zzh__2020@outlook.com>
2025-09-29 18:15:45 +0800

    Initial commit

---
+++ src/main/java/kim/pvp/kevin16/plugins/NextEnchantments.java
@@ -0,0 +1,17 @@
+package kim.pvp.kevin16.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NextEnchantments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
