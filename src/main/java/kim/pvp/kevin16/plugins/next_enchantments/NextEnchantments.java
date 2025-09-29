fb3417de43512a25c177bf1ebe68892c8f8f873a
SuperKevinZhou <zzh__2020@outlook.com>
2025-09-29 20:10:55 +0800

    Add 'Rage' enchantment

--- src/main/java/kim/pvp/kevin16/plugins/next_enchantments/NextEnchantments.java
+++ src/main/java/kim/pvp/kevin16/plugins/next_enchantments/NextEnchantments.java
@@ -2,12 +2,29 @@ package kim.pvp.kevin16.plugins.next_enchantments;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public final class NextEnchantments extends JavaPlugin {
+
+    private static final Map<Integer, String> ENCHANTMENTS_MAP;
+
+    static {
+        Map<Integer, String> tempMap = new HashMap<>();
+        tempMap.put(0, "Rage");
+//        tempMap.put(1, "Vampire");
+//        tempMap.put(2, "BloodDrinking");
+//        tempMap.put(3, "PeaceMaker");
+        ENCHANTMENTS_MAP = Collections.unmodifiableMap(tempMap);
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("NextEnchantments Plugin has been enabled!");
+        saveResource("lang.yml", false);
+        saveResource("enchantments.yml", false);
     }
 
     @Override
